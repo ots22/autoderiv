fa60e52f0cdc3426eb44d2220076ffc9e21cb673
Oliver Strickson <ots22@cam.ac.uk>
2018-02-23 18:09:38 +0000

    Instrument more-or-less working as intended

--- racket/instrument.rkt
+++ racket/instrument.rkt
@@ -13,36 +13,37 @@
   (displayln form)
   form)
 
-;; tape-helper works on expanded forms
-(define-syntax (tape-helper/orig stx)
-  (syntax-parse stx #:literals (#%plain-app quote)
-                [(_) #'()]
-                [(_ (args ...)) #'(tape-helper/orig args ...)] ;; descend into lists
-                [(_ #%plain-app f args ...) #'(add-to-tape (apply f (tape-helper/orig args ...)))]
-                [(_ quote arg) #'(add-to-tape (quote arg))]
-                [(_ arg1 rest ...+) #'(list (tape-helper/orig arg1) (tape-helper/orig rest ...))] ;; two or more
-                [(_ arg1) #'(add-to-tape arg1)]))
-;; fixed for quote, but this way will need to deal with every special form (e.g still fails for if)
-;; want to pass through everything (except quote?) - seems hard to make a general statement
-;; there are some special functions for handling this sort of thing in
-;; one of the syntax modules (stx(?) something like kernel forms)
-;; xxxxFIXED currently fails on 'quote':  (tape-helper (quote 5)) => (tape-helper quote 5)
 
-;; another attempt (doesn't work)
-(define-syntax (tape-helper stx)
-  (displayln "in-helper")
-  (syntax-parse stx #:literals (#%plain-app)
-                [(_) #'()]
-                [(_ (#%plain-app f args ...)) #'((add-to-tape (apply f (tape-helper args ...))))]
-                [(_ (arg1 args ...)) #'(arg1 (tape-helper args ...))]
-                ;;[(_ (args ...)) #'(tape-helper args ...)] ;; descend into lists
-                [(_ arg1 rest ...+) #'(list (tape-helper arg1) (tape-helper rest ...))] ;; two or more
-                [(_ arg1) #'(add-to-tape arg1)]))
+
+;; tape-helper works on expanded forms
+(define-for-syntax (tape-helper stx)
+  (syntax-parse stx #:literals (#%plain-app quote)
+    [(#%plain-app f args ...)
+     (with-syntax ([(transformed-args ...)
+                    (map tape-helper (syntax->list #'(args ...)))])
+       #'(add-to-tape (f transformed-args ...)))]
+    
+    [(args ...) (with-syntax ([(transformed-args ...)
+                               (map tape-helper (syntax->list #'(args ...)))])
+                  #'(transformed-args ...))] ;; don't recurse down anything else
+
+    [(quote arg:number) #'(add-to-tape 'value arg)]
+    [arg #'arg] ;; ignore anything else
+    ;; literals
+    ;[(_ (arg0 rest ...)) #'((tape-helper arg0) (tape-helper ...)] ;; descend into lists
+    ;[(_ args ...) #'(args ...)] ;; ignore everything else
+    ))
+
+;(tape-helper #'(#%plain-app + (#%plain-app + 1 1) 2))
 
 (define-syntax (w/helper stx)
   (syntax-case stx ()
     [(_ a) (let ([expanded (local-expand #'a 'expression '())])
-             #`(tape-helper/orig #,expanded))]))
+             (tape-helper expanded))]))
 
-(module+ test (check-values (w/helper (+ 1 2))))
+;(w/helper (+ 1 2))
+(w/helper (if #f (+ 1 2) (* (* (* 2 (if (= 0 0) 2 1) 2) 2))))
+;(w/helper ((if #t * +) 5 5))
+;(w/helper '(a b c))
+;(module+ test (check-values (w/helper (+ 1 2))))
 ;(w/helper (+ (if #f 3 4) 5))
