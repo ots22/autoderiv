85da03de9f62b46c86fd7bbd9d0388b77b27291b
Oliver Strickson <ots22@cam.ac.uk>
2018-02-25 16:16:59 +0000

    WIP

--- racket/instrument.rkt
+++ racket/instrument.rkt
@@ -6,7 +6,8 @@
          (for-syntax
            racket
            racket/syntax
-           syntax/parse))
+           syntax/parse
+           syntax/kerncase))
 
 ;; print each intermediate function evaluation
 (define (add-to-tape form)
@@ -14,26 +15,82 @@
   form)
 
 ;; tape-helper works on expanded forms
+;(define-for-syntax (tape-helper stx)
+;  (syntax-parse stx #:literals (#%plain-app quote)
+;    [(#%plain-app f args ...)
+;     (with-syntax ([(transformed-args ...)
+;                    (map tape-helper (syntax->list #'(args ...)))])
+;       #'(trace f (f transformed-args ...)))]
+;
+;    [(quote arg) #'(trace 'value arg)]
+;    
+;    [(args ...) (with-syntax ([(transformed-args ...)
+;                               (map tape-helper (syntax->list #'(args ...)))])
+;                  (displayln (syntax-local-context))
+;                  #'(trace 'other (transformed-args ...)))]
+;
+;    [arg #'arg] ;; anything else
+;    ))
+
 (define-for-syntax (tape-helper stx)
-  (syntax-parse stx #:literals (#%plain-app quote)
+  (kernel-syntax-case stx #f
     [(#%plain-app f args ...)
-     (with-syntax ([(transformed-args ...)
+     (with-syntax ([(args* ...)
                     (map tape-helper (syntax->list #'(args ...)))])
-       #'(trace f (f transformed-args ...)))]
+       (syntax/loc stx (trace f (f args* ...))))]
 
-    [(quote arg) #'(trace 'value arg)]
-    
-    [(args ...) (with-syntax ([(transformed-args ...)
-                               (map tape-helper (syntax->list #'(args ...)))])
-                  (displayln (syntax-local-context))
-                  #'(trace 'other (transformed-args ...)))]
+    [(quote form)
+     (syntax/loc stx
+       (trace 'value (quote form)))]
 
-    [arg #'arg] ;; anything else
+    [(if pred
+         true-expr
+         false-expr)
+     (with-syntax ([true-expr* (tape-helper #'true-expr)]
+                   [false-expr* (tape-helper #'false-expr)])
+       (syntax/loc stx
+         (if pred
+             true-expr*
+             false-expr*)))]
+
+    [(%#expression expr)
+     (tape-helper #'expr)]
+
+    [(begin expr ... exprn)
+     (with-syntax ([exprn* (tape-helper #'exprn)])
+       (syntax/loc stx (begin expr ... exprn*)))]
+
+    [(begin0 expr0 expr ...)
+     (with-syntax ([expr0* (tape-helper #'expr0)])
+       (syntax/loc stx (begin expr0* expr ...)))]
+
+    [(quote-syntax expr) stx]
+
+    [(let-values ([(id ...) expr0] ...)
+       expr1 ...)
+     (with-syntax ([(expr0* ...)
+                    (map tape-helper (syntax->list #'(expr0 ...)))])
+       (syntax/loc stx
+         (let-values ([(id ...) expr0] ...)
+           expr1 ...)))]
+
+    ;[(let-values ([(id ...) expr0] ...)
+    ;   expr1 ...) stx]
+
+    [(#%plain-lambda formals body ... bodyn)
+     (with-syntax ([bodyn* (tape-helper #'bodyn)])
+       (syntax/loc stx
+         (#%plain-lambda formals body ... body*)))]
+
+    ;;[(case-lambda [] ) ???]
+
+    ;;[(letrec-values ([(id ...) expr0] ...)
+    ;;   expr1 ...+)
+    ;; ???]
     
-    ;[arg #'arg] ;; ignore anything else
-    ;; literals
-    ;[(_ (arg0 rest ...)) #'((tape-helper arg0) (tape-helper ...)] ;; descend into lists
-    ;[(_ args ...) #'(args ...)] ;; ignore everything else
+    [(with-continuation-mark key-expr val-expr result-expr)
+     (with-syntax ([result-expr* (tape-helper #'result-expr)])
+       (syntax/loc stx (with-continuation-mark key-expr val-expr result-expr*)))]
     ))
 
 ;(tape-helper #'(#%plain-app + (#%plain-app + 1 1) 2))
@@ -44,9 +101,17 @@
              (tape-helper expanded))]))
 
 
+;(w/helper (+ 1 2 3))
+;(w/helper (+ (if 1 2 3) 2))
+;(w/helper (+ (and 1 2 3) (and 2 3 4)))
+;(define a (w/helper (+ (and 1 2 3) (and 2 3 4))))
+;(w/helper (begin0 1 2 3))
 
-(w/helper (+ (if 1 2 3) 2))
-(w/helper 1)
+;(w/helper (let ((x (+ 1 2))) x))
+(w/helper (let-values (((x) (+ 1 2))) x))
+
+;(and 1 2)
+;(w/helper 1)
 ;(w/helper (let ((x 1)) 1))
 ;(w/helper (if #f (+ 1 2) (* (* (* 2 (if (+ (+ 2 1) 1) 2 3) 2) 2))))
 ;(w/helper ((if #t * +) 5 5))
